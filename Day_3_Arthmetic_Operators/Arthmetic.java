94b09d905c175657651d8c85210e9f218ce42d2c
Vabie Khei Chai <vbiekhei@gmail.com>
2026-09-18 00:21:44 +0530

    Day-3

---
+++ Day_3_Arthmetic_Operators/Arthmetic.java
@@ -0,0 +1,10 @@
+
+public class Arthmetic {
+    public static void main(String args[]) {
+        int num1 = 60;
+        int num2 = 3;
+
+        int sum = num1 + num2;
+        System.out.println("Sum of num : " + sum);
+    }
+}
